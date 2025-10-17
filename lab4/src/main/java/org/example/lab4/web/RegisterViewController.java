7d94816c5dd39c21371c96808a355e33ea1b4fe2
nastiausenko <Usenko.Anastasia@lll.kpi.ua>
2025-10-18 00:53:11 +0300

    add user posts page

--- lab4/src/main/java/org/example/lab4/web/RegisterViewController.java
+++ lab4/src/main/java/org/example/lab4/web/RegisterViewController.java
@@ -1,12 +1,6 @@
 package org.example.lab4.web;
 
-import lombok.RequiredArgsConstructor;
-import org.example.lab4.entity.User;
-import org.example.lab4.security.AccessValidator;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -27,5 +21,15 @@ public class RegisterViewController {
     @GetMapping("/home")
     public String homePage() {
         return "index";
+    }
+
+    @GetMapping("/posts")
+    public String postsPage() {
+        return "posts";
+    }
+
+    @GetMapping("/add-post")
+    public String addPostPage() {
+        return "add_post";
     }
 }
