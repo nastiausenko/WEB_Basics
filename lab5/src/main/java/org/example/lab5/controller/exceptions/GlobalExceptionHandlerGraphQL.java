38a0fd4f1eea224c30bfc62a0a0015f70a955aa5
nastiausenko <Usenko.Anastasia@lll.kpi.ua>
2025-10-19 18:30:07 +0300

    add GraphQL exception handling

---
+++ lab5/src/main/java/org/example/lab5/controller/exceptions/GlobalExceptionHandlerGraphQL.java
@@ -0,0 +1,75 @@
+package org.example.lab5.controller.exceptions;
+
+import graphql.ErrorClassification;
+import graphql.GraphQLError;
+import org.apache.tomcat.websocket.AuthenticationException;
+import org.example.lab5.service.exceptions.EmailAlreadyExistsException;
+import org.example.lab5.service.exceptions.ForbiddenException;
+import org.example.lab5.service.exceptions.UserNotFoundException;
+import org.example.lab5.service.exceptions.UsernameAlreadyExistsException;
+import org.springframework.graphql.data.method.annotation.GraphQlExceptionHandler;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.net.URI;
+import java.util.List;
+
+import static org.example.lab5.controller.exceptions.ProblemDetailsUtils.getValidationErrorsProblemDetail;
+import static org.springframework.http.HttpStatus.*;
+import static org.springframework.http.ProblemDetail.forStatusAndDetail;
+
+@ControllerAdvice
+public class GlobalExceptionHandlerGraphQL extends ResponseEntityExceptionHandler {
+
+    @GraphQlExceptionHandler(UserNotFoundException.class)
+    GraphQLError handleUserNotFound(UserNotFoundException ex) {
+        return GraphQLError.newError().message(ex.getMessage())
+                .errorType(ErrorClassification.errorClassification("user-not-found"))
+                .build();
+
+    }
+
+    @ExceptionHandler(EmailAlreadyExistsException.class)
+    GraphQLError handleEmailAlreadyExistException(EmailAlreadyExistsException ex) {
+        return GraphQLError.newError().message(ex.getMessage())
+                .errorType(ErrorClassification.errorClassification("email-already-exists"))
+                .build();
+    }
+
+    @ExceptionHandler(UsernameAlreadyExistsException.class)
+    GraphQLError handleUsernameAlreadyExistException(UsernameAlreadyExistsException ex) {
+        return GraphQLError.newError().message(ex.getMessage())
+                .errorType(ErrorClassification.errorClassification("username-already-exists"))
+                .build();
+    }
+
+    @ExceptionHandler(ForbiddenException.class)
+    GraphQLError handleForbiddenException(ForbiddenException ex) {
+        return GraphQLError.newError().message(ex.getMessage())
+                .errorType(ErrorClassification.errorClassification("forbidden-exception"))
+                .build();
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    GraphQLError handleAuthenticationException(AuthenticationException ex) {
+        return GraphQLError.newError().message(ex.getMessage())
+                .errorType(ErrorClassification.errorClassification("authentication-exception"))
+                .build();
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    GraphQLError handleBadCredentialsException(BadCredentialsException ex) {
+        return GraphQLError.newError().message(ex.getMessage())
+                .errorType(ErrorClassification.errorClassification("bad-credentials-exception"))
+                .build();
+    }
+}
